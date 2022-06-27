e2c378a7764566d8e7e9ff0eec1c8e3a94e9ad8f
Jan Müller <janmueller3698@gmail.com>
2022-06-27 16:13:44 +0200

    feat: improve footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,24 +3,46 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <footer class="pa-4 text-center text-sm">
-    <!-- prettier-ignore   -->
-    <span class="text-lighter">2021 - {{ new Date().getFullYear() }} &copy; Magdalena Jirku</span>
-    <Dot class="text-lighter" />
-    <a
-      href="https://github.com/DerYeger/magdalena-jirku"
-      rel="noopener"
-      target="_blank"
-    >
-      {{ t('source') }}
-    </a>
-    <Dot class="text-lighter" />
-    <router-link
-      to="/about"
-      :title="t('button.about')"
-      active-class="!text-$primary op90"
-    >
-      {{ t('button.about') }}
-    </router-link>
+  <footer class="footer pa-4 text-xs flex flex-wrap justify-center gap-8">
+    <div>
+      <span>{{ t('pages.legal') }}</span>
+      <span class="text-lighter">
+        2021 - {{ new Date().getFullYear() }} &copy; Magdalena Jirku
+      </span>
+      <router-link
+        to="/impressum"
+        :title="t('pages.impressum')"
+        active-class="!text-$primary op90"
+      >
+        {{ t('pages.impressum') }}
+      </router-link>
+      <router-link
+        to="/legal"
+        :title="t('pages.legal')"
+        active-class="!text-$primary op90"
+      >
+        {{ t('pages.legal') }}
+      </router-link>
+    </div>
+    <div>
+      <span>Social</span>
+      <a
+        href="https://github.com/DerYeger/magdalena-jirku"
+        rel="noopener"
+        target="_blank"
+      >
+        {{ t('source') }}
+      </a>
+    </div>
   </footer>
 </template>
+
+<style scoped>
+.footer > div {
+  display: flex;
+  flex-direction: column;
+}
+.footer > div > span:first-child {
+  font-weight: bold;
+}
+</style>
