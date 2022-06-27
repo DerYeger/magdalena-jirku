de09c3a1f3d0b99267353cc2b4f055d073013981
Jan Müller <janmueller3698@gmail.com>
2022-06-27 10:28:30 +0200

    feat: create sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const color = computed(() => {
+  if (route.path === '/') {
+    return 'rgba(255, 0, 0, 0.3)'
+  }
+  return 'rgba(0, 0, 0, 0)'
+})
+</script>
+
+<template>
+  <aside class="sidebar"></aside>
+</template>
+
+<style scoped>
+.sidebar,
+.sidebar::after,
+.sidebar::before {
+  width: 12rem;
+}
+
+.sidebar {
+  height: 100%;
+}
+
+.sidebar:after,
+.sidebar::before {
+  content: '';
+  position: absolute;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+}
+.sidebar:before {
+  background-image: url('/banner.webp');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  filter: grayscale();
+  z-index: -2;
+}
+
+.sidebar:after {
+  background: v-bind(color);
+  z-index: -1;
+}
+</style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex w-full h-full">
+    <Sidebar />
+    <div class="flex-1">
+      <Header />
+      <Main>
+        <router-view />
+      </Main>
+      <Footer />
+    </div>
+  </div>
+</template>
