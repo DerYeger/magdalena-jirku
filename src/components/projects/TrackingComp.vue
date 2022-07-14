b8dec4bbbdad32cc4539083a382075fc4d16eaf2
Jan Müller <janmueller3698@gmail.com>
2022-07-14 16:19:12 +0200

    feat: add project

---
+++ src/components/projects/TrackingComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="max-w-65rem flex flex-col items-center gap-8">
+    <h1 class="font-serif">Tracking Comp</h1>
+    <h2 class="text-lighter font-serif mt--6 text-lg">January 2021</h2>
+    <div class="flex flex-col items-center justify-center gap-8 md:flex-row">
+      <video
+        controls
+        controlslist="nodownload"
+        class="w-full md:max-w-32rem display-block"
+        poster="/img/projects/tracking_comp/beautyshot.webp"
+      >
+        <source
+          src="/img/projects/tracking_comp/Bycicle_Comp_Splitscreen_1.webm"
+        />
+      </video>
+      <span class="text-center md:text-end">
+        This is the result of my first tracking project. The goal was to embed a
+        3D object into real footage. I chose an old gate in the woods with an
+        already rusted bike leaning against it. All content in this video was
+        created by me.
+      </span>
+    </div>
+  </section>
+</template>
