5c2f56374c2f0d9f40e023cab0e29696217447d0
Jan Müller <janmueller3698@gmail.com>
2023-02-02 22:07:38 +0100

    chore: fix linting issues

---
+++ src/composables/play-videos.ts
@@ -0,0 +1,11 @@
+export {}
+
+const videos = [
+  ...document.querySelectorAll<HTMLVideoElement>('.video-gallery video'),
+]
+videos.forEach((video) =>
+  video.addEventListener('mouseover', () => video.play())
+)
+videos.forEach((video) =>
+  video.addEventListener('mouseleave', () => video.pause())
+)
