487562c1cf7598ce46c85678b2d8728d8154f471
Jan Müller <janmueller3698@gmail.com>
2025-02-09 17:47:33 +0100

    feat: update text

--- src/composables/color.ts
+++ src/composables/color.ts
@@ -2,13 +2,13 @@ export function getHighlightColor(path: string, opacity = 1) {
   if (path === `/`) {
     return `rgba(0, 50, 255, ${opacity})`
   } else if (path.startsWith(`/graphic-design`)) {
-    return `rgba(255, 0, 0, ${opacity})`
+    return `rgba(7, 87, 152, ${opacity})`
   } else if (path.startsWith(`/motion-design`)) {
     return `rgba(46, 204, 113, ${opacity})`
   } else if (path.startsWith(`/photography`)) {
-    return `rgba(18, 53, 36, ${opacity})`
+    return `rgba(74, 129, 50, ${opacity})`
   } else if (path.startsWith('/drawings')) {
-    return `rgba(165, 55, 253, ${opacity})`
+    return `rgba(255, 168, 20, ${opacity})`
   }
   return `rgba(0, 0, 0, ${opacity})`
 }
