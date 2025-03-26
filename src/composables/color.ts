709ec919616df3eafedd7d57fc20b649a688f9bc
Jan Müller <janmueller3698@gmail.com>
2025-03-26 06:44:14 +0100

    feat: implement new pages

--- src/composables/color.ts
+++ src/composables/color.ts
@@ -1,10 +1,10 @@
 export function getHighlightColor(path: string, opacity = 1) {
   if (path === `/`) {
     return `rgba(0, 50, 255, ${opacity})`
-  } else if (path.startsWith(`/graphic-design`)) {
+  } else if (path.startsWith(`/design`)) {
     return `rgba(7, 87, 152, ${opacity})`
-  } else if (path.startsWith(`/motion-design`)) {
-    return `rgba(46, 204, 113, ${opacity})`
+  // } else if (path.startsWith(`/design`)) {
+  //   return `rgba(46, 204, 113, ${opacity})`
   } else if (path.startsWith(`/photography`)) {
     return `rgba(74, 129, 50, ${opacity})`
   } else if (path.startsWith('/drawings')) {
