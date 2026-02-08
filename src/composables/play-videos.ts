ec149bd441ff163ecd73cb22262c8138997d4eb9
DerYeger <janmueller3698@gmail.com>
2026-02-08 07:56:45 +0100

    chore: remove unused deps and components

--- src/composables/play-videos.ts
+++
@@ -1,5 +0,0 @@
-export {}
-
-const videos = [...document.querySelectorAll<HTMLVideoElement>('.video-gallery video')]
-videos.forEach((video) => video.addEventListener('mouseover', () => video.play()))
-videos.forEach((video) => video.addEventListener('mouseleave', () => video.pause()))
